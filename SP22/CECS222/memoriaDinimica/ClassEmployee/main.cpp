32b554892e415742cadc6c0d8c8a00534fffa643
Bal3x <bchavez_@outlook.com>
2022-03-15 19:28:54 -0400

    Add files clase

---
+++ SP22/CECS222/memoriaDinimica/ClassEmployee/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "Employee.h"
+
+using namespace::std;
+
+int main(){
+    const int SIZE = 10;
+    int n;
+    
+    Employee myArrayofEmployee[SIZE];
+    
+    cout << "Entre la cantidad de Empleados 1 - 10: ";
+    cin >> n;
+    getDataEmployee
+    
+    return 0;
+}
+
+    
+
